a0364658f401d7f6ce2d18667661057d908d27b7
Юрий <zaitsevy@gmail.com>
2021-12-09 11:26:54 +0300

    Загрузка и использование более свежей версии Transaq Connector

--- setup.py
+++ setup.py
@@ -1,31 +1,36 @@
-from distutils.core import setup
-import platform
 import os
+import platform
+import zipfile
 from urllib.request import urlopen
 from warnings import warn
 
-assert(platform.os == 'Windows')
+import setuptools
 
-url = "http://www.finam.ru/files/"
-dll_file = 'txmlconnector64.dll' if platform.machine() == 'AMD64' \
-    else 'txmlconnector.dll'
-if not os.path.exists(dll_file):
+assert(platform.system() == 'Windows')
+
+dll = 'x64/txmlconnector64.dll' if platform.machine() == 'AMD64' \
+    else 'win32/txmlconnector.dll'
+if not os.path.exists(dll):
     warn("Transaq Connector DLL not found, downloading from Finam")
-    open(dll_file, 'wb').write(urlopen(url + dll_file).read())
+    zip = "TXmlConnector.zip"
+    url = "https://files.comon.ru/usercontent/" + zip
+    open(zip, 'wb').write(urlopen(url).read())
+    zipfile.ZipFile(zip, 'r').extractall()
 
-setup(
+setuptools.setup(
     name='transaq_connector',
-    version='1.0',
+    version='1.1',
     packages=['transaq'],
     package_dir={'transaq': '.'},
-    package_data={'transaq': [dll_file]},
+    package_data={'transaq': [dll]},
     requires=['eulxml', 'lxml'],
-    url='https://github.com/roman-voropaev/transaq_connector',
+    url='https://github.com/ZaitsevY/transaq_connector',
     license='BSD',
     author='Roman Voropaev',
     author_email='voropaev.roma@gmail.com',
     description='Python Transaq XML Connector',
     platforms=['Windows'],
+    python_requires='>=3.6',
     long_description=u"TRANSAQ Connector представляет собой открытый\
         программный интерфейс TRANSAQ (API), который позволяет\
         подключать к торговому серверу TRANSAQ собственные приложения.",
