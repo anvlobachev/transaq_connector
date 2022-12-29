cb93f981fd39866be26f798ff3b85a54f23bec8c
Viktor Krasikov <viktor.krasikov@gmail.com>
2022-12-30 01:35:44 +0800

    В TransaqConnection добавлены функции обработки сообщений от сервера

--- transaqpy.py
+++ transaqpy.py
@@ -23,6 +23,27 @@ class TransaqConnection():
         if not os.path.exists(logdir):
             os.mkdir(logdir)
 
+        # словарь соответствий типов сообщений от сервера и функций, обрабатывающих эти сообщения
+        self.__msg_handlers = {
+            ss.BoardPacket.__name__: self.on_board_packet,
+            ss.CandleKindPacket.__name__: self.on_candle_kind_packet,
+            ss.ClientAccount.__name__: self.on_client_account,
+            ss.CreditAbility.__name__: self.on_credit_ability,
+            ss.ClientOrderPacket.__name__: self.on_client_order_packet,
+            ss.HistoryCandlePacket.__name__: self.on_history_candle_packet,
+            ss.HistoryTickPacket.__name__: self.on_history_tick_packet,
+            ss.MarketPacket.__name__: self.on_market_packet,
+            ss.PositionPacket.__name__: self.on_position_packet,
+            ss.QuotationPacket.__name__: self.on_quotation_packet,
+            ss.QuotePacket.__name__: self.on_quote_packet,
+            ss.SecurityPacket.__name__: self.on_security_packet,
+            ss.SecurityPitPacket.__name__: self.on_security_pit_packet,
+            ss.SecInfoUpdate.__name__: self.on_sec_info_update,
+            ss.ServerStatus.__name__: self.on_server_status,
+            ss.TextMessagePacket.__name__: self.on_text_message_packet,
+            ss.TradePacket.__name__: self.on_trade_packet,
+        }
+
         cmd.initialize(logdir, loglevel, self.__msg_handler)
 
     def __del__(self):
@@ -35,31 +56,12 @@ class TransaqConnection():
         cmd.uninitialize()
 
     def __msg_handler(self, obj):
-        if isinstance(obj, ss.SecurityPacket):
-            pass
-        elif isinstance(obj, ss.SecurityPitPacket):
-            pass
-        elif isinstance(obj, ss.SecInfoUpdate):
-            pass
-        elif isinstance(obj, ss.BoardPacket):
-            pass
-        elif isinstance(obj, ss.MarketPacket):
-            pass
-        elif isinstance(obj, ss.CandleKindPacket):
-            pass
-        elif isinstance(obj, ss.ClientTradePacket):
-            pass
-        elif isinstance(obj, ss.ClientAccount):
-            pass
-        elif isinstance(obj, ss.CreditAbility):
-            pass
-        elif isinstance(obj, ss.ServerStatus):
-            status = 'Connected' if obj.connected else 'Disconnected'
-            print(
-                f"SERVER STATUS CHANGED: {status}")
-            self.connected = obj.connected
-        else:
-            print(obj)
+        # определяем, какой тип пакета пришёл от сервера
+        packet_class_name = type(obj).__name__
+        # определяем, какую функцию надо вызвать для обработки этого пакета
+        fn = self.__msg_handlers.get(packet_class_name, self.on_unknown_packet)
+        # асинхронно вызываем эту функцию и передаем пакет в качестве параметра
+        asyncio.run(fn(obj))
 
     def connect(self, login, password, server, min_delay=100):
         """
@@ -83,3 +85,65 @@ class TransaqConnection():
                 if self.connected:
                     return True
         return self.connected
+
+    # Функции-обработчики пакетов, отправленных с сервера.
+    # При необходимости их нужно переопределить в классе-наследнике.
+
+    async def on_board_packet(self, packet: ss.BoardPacket):
+        ...
+
+    async def on_candle_kind_packet(self, packet: ss.CandleKindPacket):
+        ...
+
+    async def on_client_account(self, packet: ss.ClientAccount):
+        ...
+
+    async def on_credit_ability(self, packet: ss.CreditAbility):
+        ...
+
+    async def on_client_order_packet(self, packet: ss.ClientOrderPacket):
+        ...
+
+    async def on_history_candle_packet(self, packet: ss.HistoryCandlePacket):
+        ...
+
+    async def on_history_tick_packet(self, packet: ss.HistoryTickPacket):
+        ...
+
+    async def on_market_packet(self, packet: ss.MarketPacket):
+        ...
+
+    async def on_position_packet(self, packet: ss.PositionPacket):
+        ...
+
+    async def on_quotation_packet(self, packet: ss.QuotationPacket):
+        ...
+
+    async def on_quote_packet(self, packet: ss.QuotePacket):
+        ...
+
+    async def on_security_packet(self, packet: ss.SecurityPacket):
+        ...
+
+    async def on_security_pit_packet(self, packet: ss.SecurityPitPacket):
+        ...
+
+    async def on_sec_info_update(self, packet: ss.SecInfoUpdate):
+        ...
+
+    async def on_server_status(self, packet: ss.ServerStatus):
+        status = 'Connected' if packet.connected else 'Disconnected'
+        print(f"SERVER STATUS CHANGED: {status}")
+        self.connected = packet.connected
+
+    async def on_text_message_packet(self, packet: ss.TextMessagePacket):
+        ...
+
+    async def on_trade_packet(self, packet: ss.TradePacket):
+        ...
+
+    async def on_unknown_packet(self, packet):
+        """
+        Функция обработки сообщений, не предусмотренных документацией.
+        """
+        print(packet)
